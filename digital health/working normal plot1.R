3f7de8ef118c2ad7e8681632a1f9578ce41bd9d6
MahmoudSalih96 <147703626+MahmoudSalih96@users.noreply.github.com>
2024-02-08 10:22:13 +0100

    Add files via upload

---
+++ digital health/working normal plot1.R
@@ -0,0 +1,21 @@
+View(trial1)
+variable.names(trial1$cups.of.coffee)
+variable.names(trial1$cups.of.tea.per.week)
+variable.names(trial1$cans.of.energy.drinks.per.week)
+variable.names(trial1$cans.of.soda.per.week)
+variable.names(trial1$average.hours.if.sleep.week)
+
+ggplot(trial1, aes(x = cups.of.coffee,
+                   y = average.hours.if.sleep.week)) +
+  geom_line()
+ggplot(trial1, aes(x = cups.of.tea.per.week,
+                   y = average.hours.if.sleep.week)) +
+  geom_line()
+ggplot(trial1, aes(x = cans.of.energy.drinks.per.week,
+                   y = average.hours.if.sleep.week)) +
+  geom_line()
+ggplot(trial1, aes(x = cans.of.soda.per.week,
+                   y = average.hours.if.sleep.week)) +
+  geom_line()
+
+
